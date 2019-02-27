44f08de5f70ff4f805829b14e3e9d9beffd95054
Gerard <gerardbajona@gmail.com>
2019-02-27 23:46:30 +0100

    Allow only integers input

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-# Author : Gerard Bajona
-# Date   : 23/02/2019
+# Author   : Gerard Bajona
+# Created  : 23/02/2019
+# Modified : 27/02/2019
 
 subdirectories() {
 	printf "Guessing Game in Bash\n\n"
@@ -25,16 +26,18 @@ guessing() {
 	printf "Guess the number of files of the %s\n\n" "$subdirsv"
 	attempts=1
 	while read -r -p "Attempt $attempts → Type a number: " answer; do
-		if [[ "$answer" -lt "$numfiles" ]] 2>/dev/null; then
-			printf "\033[33m%s \n\n\033[0m" "Too low!"
-			attempts=$((attempts + 1))
-		elif [[ "$answer" -gt "$numfiles" ]] 2>/dev/null; then
-			printf "\033[33m%s \n\n\033[0m" "Too high!"
-			attempts=$((attempts + 1))
-		elif [ "$answer" -eq "$numfiles" ] 2>/dev/null; then
-			printf "\033[32m%s \n\n\033[0m" "Congratulations! You win!"
-			printf "Have a nice day ;-)\n"
-			exit 0
+		if [[ "$answer" =~ ^[0-9]+$ ]]; then
+			if [[ "$answer" -lt "$numfiles" ]] 2>/dev/null; then
+				printf "\033[33m%s \n\n\033[0m" "Too low!"
+				attempts=$((attempts + 1))
+			elif [[ "$answer" -gt "$numfiles" ]] 2>/dev/null; then
+				printf "\033[33m%s \n\n\033[0m" "Too high!"
+				attempts=$((attempts + 1))
+			elif [ "$answer" -eq "$numfiles" ] 2>/dev/null; then
+				printf "\033[32m%s \n\n\033[0m" "Congratulations! You win!"
+				printf "Have a nice day ;-)\n"
+				exit 0
+			fi
 		else
 			printf "\033[31m%s \n\n\033[0m" "Bad answer..."
 			attempts=$((attempts + 1))
